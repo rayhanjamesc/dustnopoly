78587538aba494679ebbd0a6acc2f04b595b506c
rayhanjamesc <123619702+rayhanjamesc@users.noreply.github.com>
2023-07-10 15:48:15 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+//Make Dice animate on click for 3 seconds
+function rollDice() {
+    const dice = [...document.querySelectorAll(".die-list")];
+    dice.forEach(die => {
+      toggleClasses(die);
+      die.dataset.roll = getRandomNumber(1, 6);
+    });
+  }
+  
+  function toggleClasses(die) {
+    die.classList.toggle("odd-roll");
+    die.classList.toggle("even-roll");
+  }
+  
+  function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  document.getElementById("roll-button").addEventListener("click", rollDice);
+  
+//Give out random number from dice
+
+//Based on random number, the card result will pop out
+
+//Make certain tiles pop up on click
+
+//Make the "?" "!" popup a carousel for explanation
